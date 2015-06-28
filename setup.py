75c19acf764dda7fc9d949c165333b4137d08231
Jon Chen <bsd@voltaire.sh>
2015-06-28 10:32:32 -0700

    0.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='classdirectory',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
     license='MIT',
